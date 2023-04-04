aabffdb5f6cb92f53142f87a87d88e6f0a48e3fc
GuiEstevam <contato.estevamdelima@gmail.com>
2023-04-04 13:30:06 -0300

    Configurações iniciais do novo projeto

---
+++ app/Http/Controllers/ComplianceController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ComplianceController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function create()
+    {
+    }
+}
