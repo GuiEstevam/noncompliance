68345732eabb4ef66eec4f343cbd1978eb32d70a
GuiEstevam <contato.estevamdelima@gmail.com>
2023-04-10 10:53:22 -0300

    Configuração de horários Laravel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
+        Config::set('app.locale', 'pt_BR');
+        date_default_timezone_set(Config::get('app.timezone'));
+        Carbon::setLocale('pt_BR');
     }
 }
